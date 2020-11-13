aed7f39b5f5e4587cd03bbec41de8f1b891fbf72
YasserYka <yasseryka@gmail.com>
2020-11-13 20:10:17 +0300

    skeleton card-plugin

---
+++ plugin/card.js
@@ -0,0 +1,41 @@
+"use strict";
+
+module.exports = class Card {
+
+    image(src){
+
+    }
+
+    title(text){
+
+    }
+
+    background(hex){
+    
+    }
+
+    text(text){
+
+    }
+
+    body(text){
+
+    }
+
+    list(items){
+
+    }
+
+    anchor(url, text){
+
+    }
+
+    header(text){
+
+    }
+
+    footer(text){
+
+    }
+
+}
