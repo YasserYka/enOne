9d589365b26c4f9df1fa892a33c9f85b9ec26ba5
YasserYka <yasseryka@gmail.com>
2020-11-14 14:56:18 +0300

    varables for card class and empty builder class

--- plugin/card.js
+++ plugin/card.js
@@ -4,30 +4,32 @@ module.exports = class Card {
 
     image(src){
 
+        this.imageSrc = src; 
     }
 
     title(text){
 
+        this.cardTitle = text;
     }
 
     background(hex){
     
+        this.backgroundColor = hex;
     }
 
     text(text){
 
-    }
-
-    body(text){
-
+        this.bodyText = text;
     }
 
     list(items){
 
+        this.listItems = items;
     }
 
     anchor(url, text){
 
+        this.anchor = { url: url, text: text ? text : url };
     }
 
     header(text){
