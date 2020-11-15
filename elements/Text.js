f5fea7a80ec10f458bb57df1476df5df00e02d4d
YasserYka <yasseryka@gmail.com>
2020-11-15 17:45:12 +0300

    removed classes in plugin package and added elements package and setup for anchor and text class

---
+++ elements/Text.js
@@ -0,0 +1,7 @@
+"use strict";
+
+module.exports = class Text {
+
+    constructor(text){ }
+
+}
---
+++ elements/anchor.js
@@ -0,0 +1,7 @@
+"use strict";
+
+module.exports = class Anchor {
+
+    constructor(url, text){ }
+
+}
