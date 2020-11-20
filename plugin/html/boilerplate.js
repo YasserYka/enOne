4745ae8af8e961aca0f0e1eb9d4ddca1e50f0a2f
YasserYka <yasseryka@gmail.com>
2020-11-20 23:14:25 +0300

    fa cdn and box shadow

--- plugin/html/boilerplate.js
+++ plugin/html/boilerplate.js
@@ -12,10 +12,13 @@ const get = card => {
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
             
                 <!-- Bootstrap CSS -->
-                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-                
+                <link rel = "stylesheet" href = "https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">                
+                <!-- Font Awesome -->
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
                 <style>
                     html, body { 
+                        z-index: 1;
                         height:100%
                     }           
                     body {
@@ -24,6 +27,9 @@ const get = card => {
                     } 
                     .list-group-item {
                         background-color: transparent;
+                    }
+                    .card {
+                        box-shadow: 0 .5rem 1rem rgba(0,0,0,.70) !important;
                     }
                 </style>
                 
@@ -35,7 +41,7 @@ const get = card => {
                 <!-- JavaScript -->
                 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
             </body>
         </html>
     `;
