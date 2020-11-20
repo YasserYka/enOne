7acf6597036c73cbffe7144586b5e01f0a9d1bc6
YasserYka <yasseryka@gmail.com>
2020-11-21 00:06:42 +0300

    list file

---
+++ elements/list.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const Conatiner = require('./containers/container');
+
+module.exports = class List extends Conatiner {
+
+    constructor() {
+        
+        super();
+    }
+
+    // check the type of child because some objects can't be nested
+    safeType(child) {
+
+        return child.constructor.name === 'ListItem';
+    }
+
+    toHtml() {
+
+        return `
+            <ul class="list-group list-group-flush">
+                ${this.childrenToHtml()}
+            </ul>
+        `;
+    }
+
+}
