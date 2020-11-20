05ca35a18bd7d9cda69992722bdb4cef36b6d393
YasserYka <yasseryka@gmail.com>
2020-11-21 00:47:50 +0300

    setup package stylie and textcolor file

---
+++ stylie/textcolor.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**
+ * @param {color} bgcolor is a background color
+ */
+// TODO: map bg color to apropiate text color
+const mapper = bgcolor => {
+
+}
+
+module.exports = Object.freeze({
+    BLUE_TEXT: "text-primary",
+    GRAY_TEXT: "text-secondary",
+    GREEN_TEXT: "text-success",
+    RED_TEXT: "text-danger",
+    YELLOW_TEXT: "text-warning",
+    LIGHT_BLUE_TEXT: "text-info",
+    WHITE_TEXT: "text-white",
+    DARK_TEXT: "text-dark"
+}); 
