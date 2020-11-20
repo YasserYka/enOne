c54b6a74c571042900bcba316e4e1af2b7f1b432
YasserYka <yasseryka@gmail.com>
2020-11-20 15:01:57 +0300

    engine class

---
+++ plugin/engine.js
@@ -0,0 +1,18 @@
+
+const generatefile = require('./html/generatefile');
+const boilerplate = require('./html/boilerplate');
+const browser = require('./browser');
+
+/**
+ * @param {plugin} plugin object
+ */
+const engine = plugin => {
+    
+    generatefile.generate(boilerplate.get(plugin._card.toHtml()));
+
+    browser.lanuch(generatefile.getPath());
+}
+
+module.exports = {
+    engine: engine
+};
