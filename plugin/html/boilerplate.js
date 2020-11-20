0cf02780d2cf30eb885698652c824fa3ecc7ede3
YasserYka <yasseryka@gmail.com>
2020-11-20 16:40:35 +0300

    added styling for card to center card

--- plugin/html/boilerplate.js
+++ plugin/html/boilerplate.js
@@ -13,7 +13,19 @@ const get = card => {
             
                 <!-- Bootstrap CSS -->
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-            
+                <style>
+                
+                    body {
+                        display:flex;
+                        align-items:center;
+                    } 
+                    html, body { 
+                        height:100%
+                    } 
+
+                </style>  
+            </head>
+            <body>
                 <!-- P3rsonal Card -->
                 ${card}
             
