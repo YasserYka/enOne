112dd207e7ed70592c37c4a72931b2b91704b0ab
YasserYka <yasseryka@gmail.com>
2020-11-21 12:25:39 +0300

    function, updated sizr range for fa file

--- elements/fontawesome.js
+++ elements/fontawesome.js
@@ -4,17 +4,43 @@ const TextualConatiner = require('./containers/textualcontainer');
 
 module.exports = class Footer extends TextualConatiner {
 
+    /**
+     * @param {string} icon name (e.g., camera) 
+     */
     constructor(icon) {
         super();
 
         this.icon = icon;
     }
 
+    /**
+     * @param {string} icon name (e.g., camera)
+     * @param {number} size of name (e.g., camera)
+     */
+    constructor(icon, size) {
+        super();
+
+        if(typeof size != "number" || size < 1 || size > 5)
+            throw new Error('size must be of type int or size must be of type between 1 and 5')
+
+        this.icon = icon;
+        this.size = this.getSizeFor(size);
+    }
+
+    getSizeFor(size){
+        return {
+            1: "fa-sm", 
+            2: "fa-lg",
+            3: "fa-2x",
+            4: "fa-3x",
+            4: "fa-5x"
+        }[size];
+    }
+
     toHtml() {
 
-        // TODO: editable size
         return `
-            <i class="fa fa-${this.icon}" aria-hidden="true"></i>
+            <i class="fa fa-${this.icon} ${this.size || ''}" aria-hidden="true"></i>
         `;
     }
 
