500e95b6b7eaf9d6a006689e4a91f6d882148619
YasserYka <yasseryka@gmail.com>
2020-11-28 18:37:33 +0300

    get-http for npm api

--- sandbox/packages/manager.js
+++ sandbox/packages/manager.js
@@ -1,8 +1,19 @@
+const got = require('got');
+
 // minimal weekly installation count of packages for security reasons
 const MIN_INSTALLATION_COUNT = 10000;
 
 // checks if package is popular before installation
 const isPopular = async package => {
+
+    return got(`https://api.npmjs.org/downloads/point/last-month/${package}`).then(response => {
+    
+        return MIN_INSTALLATION_COUNT < JSON.parse(response.body).downloads;
+    }).catch(error => {
+
+        console.error(error);
+        return false;
+    });
 
 }
 
