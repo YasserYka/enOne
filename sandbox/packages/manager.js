cc45351daf82d4f54aa8f8ab8b0c99d9b1aff778
YasserYka <yasseryka@gmail.com>
2020-11-28 18:33:13 +0300

    setup manager-packages class

---
+++ sandbox/packages/manager.js
@@ -0,0 +1,20 @@
+// minimal weekly installation count of packages for security reasons
+const MIN_INSTALLATION_COUNT = 10000;
+
+// checks if package is popular before installation
+const isPopular = async package => {
+
+}
+
+// chick if package is already instaled in node_module
+const isInstalled = code => {
+
+    return false;
+}
+
+const install = package => {
+
+}
+
+module.exports = {
+};
