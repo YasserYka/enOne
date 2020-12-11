365cc926c46476c869283dd0a6651b2312fb7ff6
YasserYka <yasseryka@gmail.com>
2020-12-11 21:26:36 +0300

    env in engine

--- plugin/engine.js
+++ plugin/engine.js
@@ -1,16 +1,29 @@
-
-const generatefile = require('./html/generatefile');
+const generatefile = require('./generatefile');
 const boilerplate = require('./html/boilerplate');
 const browser = require('./browser');
+const environment = require('./environment');
+const fs = require('fs');
+const preprocessor = require('./release/preprocessor');
 
-/**
- * @param {plugin} plugin object
- */
-const engine = plugin => {
-    
-    generatefile.generate(boilerplate.get(plugin._card.toHtml(), plugin._textcolor));
+const engine = (plugin, env) => {
 
-    browser.lanuch(generatefile.getPath());
+    environment.RELEASE == env ? release(): test(plugin);
+}
+
+const test = plugin => {
+    generatefile.generate(boilerplate.get(plugin.toHtml()), environment.TESTING);
+
+    browser.lanuch(generatefile.getPath(environment.TESTING));
+}
+
+const release = () => {
+
+    fs.readFile(module.parent.filename, 'utf8', (err, file) => {
+        if (err) console.error(err)
+
+        generatefile.generate(preprocessor.preprocess(file), environment.RELEASE);
+    });
+
 }
 
 module.exports = {
