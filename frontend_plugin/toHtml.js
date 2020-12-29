cd0b9e752b4887a8d0099e8fef1441d5d4cee183
YasserYka <yasseryka@gmail.com>
2020-12-29 19:12:19 +0300

    empty functions for each element

---
+++ frontend_plugin/toHtml.js
@@ -0,0 +1,48 @@
+
+const anchor = () => {
+}
+
+const body = () => {
+}
+
+const Bold = () => {
+}
+
+const Card = () => {
+}
+
+const FontAwesome = () => {
+}
+
+const Footer = () => {
+}
+
+const Header = () => {
+}
+
+const Image = () => {
+}
+
+const List = () => {
+}
+
+const ListItem = () => {
+}
+
+const ListItem = () => {
+}
+
+const Paragraph = () => {
+}
+
+const Table= () => {
+}
+
+const TableItem= () => {
+}
+
+const Text= () => {
+}
+
+const Title= () => {
+}
