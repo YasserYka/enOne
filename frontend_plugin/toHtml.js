5146050439d5e00cc40d6b3fe01a2a8a31ecd2ea
YasserYka <yasseryka@gmail.com>
2020-12-29 20:00:33 +0300

    list, listItem, paragraph and table  functions

--- frontend_plugin/toHtml.js
+++ frontend_plugin/toHtml.js
@@ -24,7 +24,7 @@ const bold = text => {
     `;
 }
 
-const card = (children) => {
+const card = children => {
 
     return `
         <div class="card mt-2 shadow bg-dark text-center">
@@ -33,14 +33,14 @@ const card = (children) => {
     `;
 }
 
-const fontAwesome = (icon) => {
+const fontAwesome = icon => {
 
     return `
         <i class="fa fa-${icon}"></i>
     `;
 }
 
-const Footer = (children) => {
+const Footer = children => {
     
     return `
         <div class="card-footer">
@@ -58,23 +58,56 @@ const Header = () => {
     `;
 }
 
-const Image = (src) => {
+const Image = src => {
 
     return `
         <img src="${src}" class="card-img-top" alt="plugin image" />
     `;
 }
 
-const List = () => {
+const List = children => {
+
+    return `
+        <ul class="list-group list-group-flush">
+            ${childrenToHtml(children)}
+        </ul>
+    `;
 }
 
-const ListItem = (item) => {
+const ListItem = item => {
+
+    return `
+        <a target="_blank" href="${item.url}" class="list-group-item list-group-item-action"> 
+            ${item.text}
+        </a>
+    `;
 }
 
-const Paragraph = (text) => {
+const Paragraph = text => {
+
+    return `
+        <p class="card-text">
+            ${text}
+        </p>
+    `;
 }
 
 const Table = () => {
+
+    return `
+        <div class="table-responsive">
+            <table class="table table-borderless table-sm table-dark">
+                <thead>
+                    <tr>
+                        ${children[0].getHead()}
+                    </tr>
+                </thead>
+                <tbody>
+                    ${childrenToHtml(children)}
+                </tbody>
+            </table>
+        </div>
+    `;
 }
 
 const TableItem = (item) => {
