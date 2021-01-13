61628728d962de4e3ba7456adcbd0d108a12940f
YasserYka <yasseryka@gmail.com>
2021-01-13 20:30:11 +0300

    note example

---
+++ plugin/examples/note.js
@@ -0,0 +1,13 @@
+import plugin from '../plugin.js';
+import engine from '../engine.js';
+import environment from '../environment.js';
+
+
+// display a title in card's body
+plugin.header(plugin.text('Note'));
+
+// adds a list in card's body
+plugin.input('Your thoughts?');
+
+// compile and launch the plugin
+engine.engine(plugin, environment.TESTING);
