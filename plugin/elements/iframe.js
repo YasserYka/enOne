c0ad198e69a632ea14a9ce0a634464a7d7cd143f
YasserYka <yasseryka@gmail.com>
2021-01-15 20:34:41 +0300

    iframe element

---
+++ plugin/elements/iframe.js
@@ -0,0 +1,19 @@
+import Container from "./container.js";
+
+export default class IFrame extends Container {
+
+    constructor() {
+        
+        super();
+    }
+
+    toHtml() {
+
+        return `
+            <div class="embed-responsive embed-responsive-4by3">
+                <iframe class="embed-responsive-item" width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="${this.url}"></iframe>
+            </div>
+        `;
+    }
+
+}
