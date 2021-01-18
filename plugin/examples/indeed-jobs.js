7ecc38a230e863c87e3c929fa8a0b6cb4f4e1e6c
YasserYka <yasseryka@gmail.com>
2021-01-18 22:07:10 +0300

    updated indeed example

--- plugin/examples/indeed-jobs.js
+++ plugin/examples/indeed-jobs.js
@@ -1,23 +1,34 @@
+// you can import libraries you need
+const indeed_scraper = require('indeed-scraper');
 const plugin = require('../plugin.js');
 const engine = require('../engine.js');
 const environment = require('../environment.js');
 
-// display an anchor in card's title
-plugin.header(plugin.text('Today\'s jobs'));
+indeed_scraper.query({
+    host: 'www.indeed.com',
+    query: 'Software',
+    city: 'Seattle, WA',
+    radius: '25',
+    level: 'entry_level',
+    jobType: 'fulltime',
+    maxAge: '7',
+    sort: 'date',
+    limit: 8
+}).then(jobs => { 
+    // display an anchor in card's title
+    plugin.header(plugin.anchor('https://opensource.indeedeng.io/api-documentation/', 'Indeed Scraper'));
 
-// create a table from an array of objects 
-plugin.table(
-    [
-        {'Job title': 'Python Developer', 'Company': 'confidential', 'City': 'Van', 'WFH': 'NO'},
-        {'Job title': 'UX/UI Designer', 'Company': 'confidential', 'City': 'Sokoto', 'WFH': 'YES'},
-        {'Job title': 'Data Scientist', 'Company': 'confidential', 'City': 'Yonkers', 'WFH': 'NO'},
-    ].map(job => plugin.tableItem(job))
-);
+    jobs = jobs.map(job => (({title, company, postDate}) => ({title, company, postDate}))(job));
 
-plugin.footer(plugin.anchor('https://opensource.indeedeng.io/api-documentation/', 'Indeed API'));
+    console.log(jobs);
 
-// small sized icon in card's header
-plugin.footer(plugin.fontawesome('briefcase', 1));
+    // create a table from an array of objects 
+    plugin.table(
+        jobs.map(job => plugin.tableItem(job))
+    );
 
-// pass your external packages used and your card object into our plugin engine
-engine.engine(plugin, environment.TESTING);
+    plugin.footer(plugin.text("Jobs in last 24H"));
+
+    // pass your external packages used and your card object into our plugin engine
+    engine.engine(plugin);
+ });
