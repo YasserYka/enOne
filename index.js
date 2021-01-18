7836c17f4ca5262b388aad1be200e7d50e01d864
YasserYka <yasseryka@gmail.com>
2021-01-18 21:21:31 +0300

    index in electron and removed tool bar

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
-  const win = new BrowserWindow({
+  const window = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true
-    }
-  })
+    },
+  });
 
-  win.loadFile('index.html')
+  window.setMenuBarVisibility(false)
+  window.loadFile('./dashboard/index.html')
 }
 
 app.whenReady().then(createWindow)
