9eab17dcdbba89dac9f17cf500f659ccddd47a20
YasserYka <yasseryka@gmail.com>
2021-01-28 23:26:20 +0300

    new interface for plugin

--- plugin/examples/weather-api.js
+++ plugin/examples/weather-api.js
@@ -1,66 +1,92 @@
-const plugin = require('../plugin.js');
-const engine = require('../engine.js');
-const environment = require('../environment.js');
+const got = window.require('got');
 
-// TODO: handle submitting packages
-const got = require('got');
+/*
+When creating class name or id to use them in script element please do it in this way
+for example from <h5 id="id"> to <h5 id={YouClassName.name + id}>
+to make this as unique as possible and avoid having duplicate id with other plugins
+and to make it easier to debug.
 
-const request = async url => {
- 
-    return await got(url).then(response => {
-        
-        return JSON.parse(response.body);
-    }).catch(error => {
+*/
 
-        throw Error(error);
-    });
-}
+module.exports = class WeatherAPI {
 
-// call weather API
-const getWeather = async woeid => {
+    // called when plugin is initiated
+    init(){
 
-    return request(`https://www.metaweather.com/api/location/${woeid}/`);
-}
+        this.weather = {
+            temperature: null,
+            image: null,
+            description: null,
+        } // defien default values
 
-// get city via IP
-const getCity = async () => {
-    
-    return request('https://freegeoip.app/json/');
-}
+        this.interval = 43200000; // 12 hours in milliseconds
 
-// metaweather's API require woeid to get weather info
-const getWoeid = async city => {
+        this.weather = getWeatherData();
+    }
 
-    return request(`https://www.metaweather.com/api/location/search/?query=${city}`);
-}
+    // update plugin in interval
+    interval(){
 
-getCity().then(city => {
+        setInterval(update(), this.weather.interval);
+    }
 
-    getWoeid(city.city).then(woeid => {
+    // updates indiviual elements
+    update(){
+        this.weather = this.getWeatherData();
 
-        getWeather(woeid[0].woeid).then(weatherdata => {
+        document.getElementById('description').innerHTML = `In London ${this.weather.description}`;
+        document.getElementById('description').innerHTML = `Temperature ${this.weather.temperature}°C`;
+        document.getElementById('description').src = this.weather.image;
+    }
 
-            weatherdata = weatherdata.consolidated_weather[0];
-        
-            // display a title in card's body
-            plugin.header(plugin.text('Today\'s Weather'));
-            
-            // display image to describe the weather
-            plugin.image(`https://www.metaweather.com/static/img/weather/png/${weatherdata.weather_state_abbr}.png`);
-        
-            // card's title
-            plugin.title(`In ${city.city} The Weather Is ${weatherdata.weather_state_name}`);
-        
-            // paragraph to display temperature
-            plugin.body(plugin.bold(`Temperature ${weatherdata.min_temp | 0}-${weatherdata.max_temp | 0}°C`));
-        
-            // URL of API document
-            plugin.footer(plugin.anchor('https://www.metaweather.com/api/', 'Weather API'));
-            
-            // compile and launch the plugin
-            engine.engine(plugin, environment.TESTING);
+    // define your own function to fetch desired data
+    getWeatherData(){
+
+        return got('https://www.metaweather.com/api/location/44418/').then(response => {
+            const weatherData = response.body;
+
+            return {
+                temperature: `${weatherData.min_temp | 0}-${weatherData.max_temp | 0}`,
+                description: weatherData.weather_state_name,
+                imageUrl: `https://www.metaweather.com/static/img/weather/png/${weatherData.weather_state_abbr}.png`
+            };
+
+        }).catch(error => {
+
+            throw Error(error);
         });
+    } 
 
-    });
+    // return JSX code to generates dom to be displayed.
+    render(){
 
-});
+        return ( 
+            <div  class="grid-stack-item" gs-w="2" gs-h="6">
+                <div class="card grid-stack-item-content mt-2 shadow bg-dark text-center">
+                    <div class="card-header">
+                        Today's Weather
+                    </div>
+
+                    <img src={this.data.image} class="card-img-top" alt="Card-Image.js" />
+
+                    <div class="card-body">
+                        <h5 id="description" class="undefined">
+                            In London {this.weather.description}
+                        </h5>
+
+                        <b id="temperature">Temperature {this.weather.temperature}°C</b>
+                    </div>
+
+                    <div class="card-footer text-white">  
+                        <a target="_blank" href="https://www.metaweather.com/api/" class="text-white card-link">
+                            Weather API
+                        </a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    // script to be called inside <script> element
+    script(){}
+}
