ace5d6df90d031ae24f15c8d8848aeaaf6dce477
YasserYka <yasseryka@gmail.com>
2021-01-29 15:38:41 +0300

    add and wrape functions for muuri

--- dashboard/js/muuri.js
+++ dashboard/js/muuri.js
@@ -1,5 +1,4 @@
-
-const grid = new Muuri(".grid", {
+const muuriGrid = new Muuri(".grid", {
   dragEnabled: true,
   layout: {
     fillGaps: true,
@@ -10,10 +9,28 @@ const grid = new Muuri(".grid", {
   },
 });
 
-function storeGrid(grid) {
-    const serializedGrid = JSON.stringify(grid.getItems().map((item) => {
-        return item.getElement().getAttribute('data-id');
-    }));
+const muuriSerialize = (grid) => {
+  const serializedGrid = JSON.stringify(
+    grid.getItems().map((item) => {
+      return item.getElement().getAttribute("data-id");
+    })
+  );
 
-    window.localStorage.setItem('layout', serializedGrid); 
+  window.localStorage.setItem("layout", serializedGrid);
+};
+
+// to wrap plugin to make them draggable
+const muuriWrap = element => {
+  const item = document.createElement('div');
+
+  item.className = "item";
+  item.innerHTML = `<div class="item-content">${element}</div>`;
+
+  return item;
+};
+
+const muuriAdd = element => {
+
+  grid.add(muuriWrap(element));
+  grid.refreshItems().layout();
 }
