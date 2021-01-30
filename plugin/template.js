ab9cd1bfeb37166a2f8b15ac2fd1cd4b359d1fe4
YasserYka <yasseryka@gmail.com>
2021-01-30 09:24:02 +0300

    replaced boilerplate w template.js

---
+++ plugin/template.js
@@ -0,0 +1,20 @@
+
+module.exports = class WeatherAPI {
+  
+  constructor() {
+
+  }
+
+  // return JSX code to generates dom to be displayed.
+  async render() {
+
+    return (
+        <div></div>
+    );
+  }
+
+  script() {
+
+  }
+  
+};
