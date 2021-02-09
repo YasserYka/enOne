dcebb75a266162d1c0369f6eda69802ab05c75e3
YasserYka <yasseryka@gmail.com>
2021-02-09 17:56:12 +0300

    removed loading logic from app

--- app.js
+++ app.js
@@ -1,27 +1,7 @@
 const loader = require('../plugin/loader');
+const PluginManager = require('./pluginManager');
 
-const initiate = () => {
-
-    const plugins = loader.load();
-
-    Promise.all(plugins.map( async plugin => {
-
-        plugin.initialize().then(() => {
-
-            plugin.render().then(renderedPlugin => {
-
-                let wrappedElement = muuriAdd(renderedPlugin);
-                
-                observeElement(wrappedElement);
-
-                plugin.script().then(() =>
-    
-                    console.log("Finished executing " + plugin.constructor.name)
-                );
-            });
-        });
-    
-    }));
-}
-
-initiate();
+(() => {
+   
+    PluginManager.load();
+})();
