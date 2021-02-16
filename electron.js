96a5e57c9d8947d500ec5c0ca3ff3375b1f71619
YasserYka <yasseryka@gmail.com>
2021-02-16 23:37:46 +0300

    stored global variables and updateCheck function

--- electron.js
+++ electron.js
@@ -1,5 +1,8 @@
 const { app, BrowserWindow } = require('electron')
 const config = require(__dirname + '/config.json');
+const got = require('got');
+
+global.__root = __dirname;
 
 function createWindow () {
   const window = new BrowserWindow({
@@ -12,11 +15,30 @@ function createWindow () {
   });
 
   window.setMenuBarVisibility(false);
-  window.loadFile('./dashboard/index.html');
+  window.loadFile('./frontend/index.html');
 
   if (config.environment === 'dev')
     window.webContents.openDevTools();
 }
+
+function updateCheck (currentVersion) {
+
+  got('https://raw.githubusercontent.com/YasserYka/enOne/master/config.json?token=ALEO7DGKQ5QIJIJDZPWSXYLAGVSVU', { json: true }).then(response => {
+      const remoteVersion = response.body.version;
+
+      if (currentVersion < remoteVersion)
+        console.log("\n\x1b[33mA new version is avaliable, your current version is " + currentVersion + " lates version is " + remoteVersion + "\x1b[0m\n");
+      
+  }).catch(error => {
+    console.log(error.response.body);
+  });
+}
+
+const currentVersion = config.version;
+
+console.log("\n\x1b[32mVersion " + currentVersion + "\x1b[0m\n");
+
+updateCheck(currentVersion);
 
 app.whenReady().then(createWindow);
 
