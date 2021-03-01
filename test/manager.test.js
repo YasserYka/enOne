673ace3483aa8f7c183c87a8feec308c6ec9d219
yasseryka <yasseryka@gmail.com>
2021-03-01 07:01:14 -0500

    unit tests

--- test/manager.test.js
+++ test/manager.test.js
@@ -4,8 +4,26 @@ test("Load all plugins function", async () => {
 
     manager.load();
 
-    console.log(manager.plugins);
+    manager.plugins.forEach(plugin => {
+        
+        expect(plugin).toMatchSnapshot({
+            name: expect.any(String),
+            config: expect.any(Object),
+            object: expect.any(Object)
+        });
+    });
+});
 
-    expect(manager.plugins).not.toHaveLength(0);
+test("List local plugins", () => {
 
+    const pluginNames = manager.listLocal();
+
+    expect(pluginNames).not.toHaveLength(0);
+});
+
+test("List remote plugins", () => {
+
+    const pluginNames = manager.listRemote();
+
+    expect(pluginNames).not.toHaveLength(0);
 });
--- test/util.test.js
+++ test/util.test.js
@@ -1,12 +1,19 @@
-const { checkLatestUpdate } = require("../lib/util");
+const { checkLatestUpdate, color } = require("../lib/util");
 
 test("Check latest version function", async () => {
 
     const log = jest.spyOn(global.console, 'log');
 
-    await checkLatestUpdate('0.9');
+    const currentVersion = '0.9';
+
+    await checkLatestUpdate(currentVersion);
     
     expect(log).toHaveBeenCalledWith(expect.stringMatching(/A new version is avaliable, your current version is (.*?) lates version is (.*?)/));
-    
 });
+
+test("ANSI colors function", async () => {
+
+    expect(color.blue('Message')).toBe('\x1b[34mMessage\x1b[0m');
+});
+
 
