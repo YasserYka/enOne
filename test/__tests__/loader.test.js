1a8e0845017d4a2e7564b6db8fcbedc2e753e872
yasseryka <yasseryka@gmail.com>
2021-03-10 05:18:13 -0500

    before each/all

--- test/__tests__/loader.test.js
+++ test/__tests__/loader.test.js
@@ -3,34 +3,39 @@ const { resolve } = require("path");
 
 jest.mock('fs');
 
-test("loadAll plugins function", async () => {
+beforeAll(() => {
 
-    const plugins = loadAllPlugins();
+    jest.mock(resolve(__dirname + '/../../enOne-plugins/plugins/testPlugin/config.json'), () => ({author:"AuthorName", verion:"1.0"}), { virtual: true });
+    jest.mock(resolve(__dirname + '/../../output/compiled/testPlugin.js'), () => {
 
-    expect(plugins).not.toHaveLength(0); 
+        return class testPlugin {
+            async initialize() {}
+            async render() {return "<div class=\"card text-center\" style=\"padding: 2rem;\"><h1 id=\"clock\">11:43 PM</h1><h4 id=\"day\">Tuesday</h4><h6 id=\"date\">Mar 25</h6></div>"}
+            async script() {}   
+        }
+    }, { virtual: true });
+})
+
+beforeEach(() => {
+
+    require('fs').__setMockedFiles({
+        [resolve(__dirname + '/../../output/compiled/testPlugin.js')]: true,
+        [resolve(__dirname + '/../../enOne-plugins/plugins/testPlugin/config.json')]: true,
+    });
+});
+
+describe("Load all functions", () => {
+
+    test("Successfully load all existing plugin", () => {
+
+        expect(loadAllPlugins().length).toBe(1);     
+    });
 
 });
 
 describe("Load plugin function", () => {
 
     test("Successfully load existing plugin", () => {
-
-        jest.mock(resolve(__dirname + '/../../enOne-plugins/plugins/testPlugin/config.json'), () => ({author:"AuthorName", verion:"1.0"}), { virtual: true });
-
-        jest.mock(resolve(__dirname + '/../../output/compiled/testPlugin.js'), () => {
-
-            return class testPlugin {
-                async initialize() {}
-                async render() {return "<div class=\"card text-center\" style=\"padding: 2rem;\"><h1 id=\"clock\">11:43 PM</h1><h4 id=\"day\">Tuesday</h4><h6 id=\"date\">Mar 25</h6></div>"}
-                async script() {}   
-            }
-        }, { virtual: true });
-
-        // for mocking existsSync purpose
-        require('fs').__setMockedFiles({
-            [resolve(__dirname + '/../../output/compiled/testPlugin.js')]: true,
-            [resolve(__dirname + '/../../enOne-plugins/plugins/testPlugin/config.json')]: true,
-        });
        
         const plugin = loadPlugin("testPlugin");
         
