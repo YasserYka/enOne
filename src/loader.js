b1f139232cdac7b6fefa308f73e916d738c976ef
yasseryka <yasseryka@gmail.com>
2021-03-17 05:18:22 -0400

    loader

--- src/loader.js
+++ src/loader.js
@@ -5,49 +5,31 @@ const Widget = require('./widget');
 const loadWidget = (widgetsDirectory, widgetDirectoryName) => {
  
     const widgetPath = `${widgetsDirectory}/${widgetDirectoryName}`;
-    
-    const config = require(`${widgetPath}/config.json`);
+    const configPath = widgetPath + "/config.json";
+
+    if (!fs.existsSync(configPath))
+        return new FileNotFound(configPath);
+
+    const config = require(configPath);
         
     const compiledWidgetPath = widgetPath + '/compiled.js';
+    const indexWidgetPath = widgetPath + '/index.js';
 
-    if (!fs.existsSync(compiledWidgetPath))
-        return new Error('Couldn\'t find compiled widget file for ' + widgetDirectoryName + ' at ' + widgetPath);
+    if (!fs.existsSync(compiledWidgetPath) && fs.existsSync(indexWidgetPath))
+        return new WidgetNotCompiled(widgetDirectoryName);
 
     const widgetInstance = new (require(compiledWidgetPath));  
 
-    return new Widget(widgetName, widgetInstance, config);
+    return new Widget(widgetDirectoryName, widgetInstance, config);
 }
 
 const getWidgetsInformation = () => {
 
-    const widgetsDirectory = __dirname + '/../enOne-widgets/widgets';
-
-    if (!fs.existsSync(widgetsDirectory))
-        throw new Error('Couldn\'t find widget directory: ' + widgetsDirectory);
-    
-    const widgetsInfo = [];
-
-    fs.readdirSync(widgetsDirectory).forEach(directoryName => {
-
-        const widgetPath = `${widgetsDirectory}/${directoryName}`;
-        const packageJSONPath = widgetPath + '/package.json';
-
-        if (!fs.existsSync(widgetPath + '/config.json'))
-            console.error('Widget\'s config.json file dose not exists for ' + directoryName);
-        else if (!fs.existsSync(widgetPath + '/index.js'))
-            console.error('Widget\'s index.js file dose not exists for ' + directoryName);
-        else if (!fs.existsSync(packageJSONPath))
-            console.error('Widget\'s package.json file dose not exists for ' + directoryName);
-        else 
-            widgetsInfo.push({
-                directoryName: directoryName,
-                disabled: true,
-                version: require(packageJSONPath).version
-            });
-
-    });
-
-    return widgetsInfo;
+    return fs.readdirSync(widgetsDirectory).map(directoryName => ({
+        directoryName: directoryName,
+        disabled: true,
+        dependeicesInstalled: false
+    }));
 }
 
 module.exports = {
