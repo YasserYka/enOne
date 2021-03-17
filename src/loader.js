20efe38082b410802959825cdedca4f973e77170
yasseryka <yasseryka@gmail.com>
2021-03-17 03:08:55 -0400

    refactor loader

--- src/loader.js
+++ src/loader.js
@@ -1,43 +1,39 @@
 const fs = require('fs');
 const Widget = require('./widget');
-const { resolve } = require('path');
-const { transform } = require('./compile');
 
-const WIDGETS_SUBMODULE_DIRECTORY = resolve(__dirname + "/../enOne-widgets/widgets");
-const COMPILED_WIDGETS_DIRECTORU = resolve(__dirname + "/../output");
+// loads compiled file of widget and config file from enOne-widgets folder
+const loadWidget = (widgetsDirectory, widgetDirectoryName) => {
 
-// loads compiled index file of widget and config file from enOne-widgets folder
-const load = widgetName => {
-
-    const configPath = `${WIDGETS_SUBMODULE_DIRECTORY}/${widgetName}/config.json`; 
+    const configPath = `${widgetsDirectory}/${widgetDirectoryName}/config.json`; 
 
     if (!fs.existsSync(configPath))
-        return new Error("Couldn't find widget's config file for widget " + widgetName + " at " + configPath);
+        return new Error("Widget's config file dose not exists for " + widgetDirectoryName + " at " + configPath);
 
     const config = require(configPath);
         
-    const widgetPath = `${COMPILED_WIDGETS_DIRECTORU}/${widgetName}.js`;
+    const widgetPath = `${widgetsDirectory}/compiled.js`;
 
     if (!fs.existsSync(widgetPath))
-        return new Error("Couldn't find compiled widget file");
+        return new Error("Couldn't find compiled widget file for " + widgetDirectoryName + " at " + widgetPath);
 
     const widgetInstance = new (require(widgetPath));  
 
     return new Widget(widgetName, widgetInstance, config);
 }
 
-// reads widgets folder
-const listLocal = () => {
+const getWidgetsInformation = () => {
 
-    if (!fs.existsSync(WIDGETS_SUBMODULE_DIRECTORY))
-        return new Error("Couldn't find widget directory: " + WIDGETS_SUBMODULE_DIRECTORY);
+    const widgetsDirectory = resolve(__dirname + "/../enOne-widgets/widgets");
+
+    if (!fs.existsSync(widgetsDirectory))
+        return new Error("Couldn't find widget directory: " + widgetsDirectory);
     
-    const widgetNames = fs.readdirSync(WIDGETS_SUBMODULE_DIRECTORY);
+    const widgetNames = fs.readdirSync(widgetsDirectory);
 
     return widgetNames;
 }
 
 module.exports = {
-    listLocal: listLocal,
-    loadWidgetByName: load
+    getWidgetsInformation: getWidgetsInformation,
+    loadWidget: loadWidget,
 };
