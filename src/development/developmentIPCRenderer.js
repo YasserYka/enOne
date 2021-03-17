ec3b34221742fdba6badb7ee512fff1f6950af4f
yasseryka <yasseryka@gmail.com>
2021-03-17 04:04:59 -0400

    moved files

--- src/development/developmentIPCRenderer.js
+++
@@ -1,9 +0,0 @@
-const { ipcRenderer } = require('electron');
-const { loadPlugin } = require('../loader');
-
-console.log(ipcRenderer.sendSync('synchronous-message', 'ping')) // prints "pong"
-
-ipcRenderer.on('asynchronous-reply', (event, filename) => {
-
-  const plugin = loadPlugin(filename);
-})
